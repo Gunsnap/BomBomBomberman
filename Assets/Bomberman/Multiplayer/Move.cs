f0b826d860060d3737593c1b331e23c9f7a81e8e
HvadSkerDer <ThomasKrogh-@hotmail.com>
2016-06-02 13:13:56 +0200

    move melder ingen fejl

--- Assets/Bomberman/Multiplayer/Move.cs
+++ Assets/Bomberman/Multiplayer/Move.cs
@@ -1,13 +1,18 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityStandardAssets.CrossPlatformInput;
 
 public class Move : MonoBehaviour {
 
 	float speed = 10.0f;
-	float rotationspeed = 100.0f;
-
 
 	void Update () {
-		float translatio	
+		float translation = CrossPlatformInputManager.GetAxis ("Vertical") * speed;
+		float translation2 = CrossPlatformInputManager.GetAxis ("Horizontal") * speed;
+
+		translation *= Time.deltaTime;
+		translation2 *= Time.deltaTime;
+		transform.Translate (0, translation, 0);
+		transform.Translate (translation2, 0, 0);
 	}
 }
