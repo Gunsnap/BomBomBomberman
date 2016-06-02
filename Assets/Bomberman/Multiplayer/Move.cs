830727a6446e8e799356c5b44e9cbbc1510f2827
HvadSkerDer <ThomasKrogh-@hotmail.com>
2016-06-02 12:53:43 +0200

    Level2 (multiplayer)

---
+++ Assets/Bomberman/Multiplayer/Move.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Move : MonoBehaviour {
+
+	float speed = 10.0f;
+	float rotationspeed = 100.0f;
+
+
+	void Update () {
+		float translatio	
+	}
+}
